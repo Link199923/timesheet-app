5a86f3e6694a9a92633c28afd94ded30e65d4f26
Link199923 <hcuel856@mtroyal.ca>
2026-02-20 11:07:37 -0700

    Initial commit - Timesheet App

--- app.js
+++ app.js
@@ -96,6 +96,8 @@ app.use("/timesheets", timesheetRoutes);
 // ===============================
 // Start Server
 // ===============================
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
